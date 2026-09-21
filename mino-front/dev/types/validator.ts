5fbfd701c923e4fe90a7cd2a96726d60f0d1e912
Lotta <lotta.shved@incredible-web.com>
2026-09-21 13:01:25 +0200

    added strapi

--- mino-front/dev/types/validator.ts
+++ mino-front/dev/types/validator.ts
@@ -36,7 +36,7 @@ type LayoutConfig<Route extends LayoutRoutes = LayoutRoutes> = {
 }
 
 
-// Validate ../../app/page.tsx
+// Validate ../../../app/page.tsx
 {
   type __IsExpected<Specific extends AppPageConfig<"/">> = Specific
   const handler = {} as typeof import("../../app/page.js")
@@ -51,7 +51,7 @@ type LayoutConfig<Route extends LayoutRoutes = LayoutRoutes> = {
 
 
 
-// Validate ../../app/layout.tsx
+// Validate ../../../app/layout.tsx
 {
   type __IsExpected<Specific extends LayoutConfig<"/">> = Specific
   const handler = {} as typeof import("../../app/layout.js")
